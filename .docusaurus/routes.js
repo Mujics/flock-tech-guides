477512a8d9c3c02f26c84db500b4056f8148f911
Lucas Mujica <lucasmujica@192.168.1.4>
2024-07-17 16:22:13 -0300

    Remove build files from old version

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/flock-tech-guides/__docusaurus/debug',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug', 'c5f'),
-    exact: true
-  },
-  {
-    path: '/flock-tech-guides/__docusaurus/debug/config',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug/config', '410'),
-    exact: true
-  },
-  {
-    path: '/flock-tech-guides/__docusaurus/debug/content',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug/content', '07c'),
-    exact: true
-  },
-  {
-    path: '/flock-tech-guides/__docusaurus/debug/globalData',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug/globalData', 'b5e'),
-    exact: true
-  },
-  {
-    path: '/flock-tech-guides/__docusaurus/debug/metadata',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug/metadata', '044'),
-    exact: true
-  },
-  {
-    path: '/flock-tech-guides/__docusaurus/debug/registry',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug/registry', 'dc7'),
-    exact: true
-  },
-  {
-    path: '/flock-tech-guides/__docusaurus/debug/routes',
-    component: ComponentCreator('/flock-tech-guides/__docusaurus/debug/routes', '139'),
-    exact: true
-  },
-  {
     path: '/flock-tech-guides/',
     component: ComponentCreator('/flock-tech-guides/', '683'),
     routes: [
--- build/assets/js/f3ff8aec.24111a8f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfedpat_docs=self.webpackChunkfedpat_docs||[]).push([[275],{1966:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
